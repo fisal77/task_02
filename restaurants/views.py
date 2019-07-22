57ca11d9b07041b3714845208d605f5b50b933b5
fisal77 <fisal0707@gmail.com>
2019-07-22 18:56:45 +0300

    first commit

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def bootcamp77(request):
+	context = {
+		"msg": "Al-Baik resturant",
+		"title": "Spicy checken"
+	}
+	return render(request, 'albaik.html', context)
